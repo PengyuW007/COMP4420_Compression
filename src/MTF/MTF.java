bcc7204d6062bcc4ff350930c9773e952224456e
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-04-10 22:53:28 -0500

    Create MTF.java

---
+++ src/MTF/MTF.java
@@ -0,0 +1,48 @@
+package MTF;
+
+import java.util.*;
+
+public class MTF {
+    static ArrayList<Character> Alphabet = alphabet();
+
+    public static void main(String[] args) {
+        String S = "INEFFICIENCIES";
+        ArrayList<String>mtf = mtf(S);
+
+        for(int i = 0;i<Alphabet.size();i++){
+            System.out.println(Alphabet.get(i));
+        }
+
+    }
+
+    public static ArrayList<String>mtf(String S){
+        int len = S.length();
+        ArrayList<String>res = new ArrayList<>();
+        for(int i = 0;i<len;i++){
+            for(int j = 0;j<26;j++){
+                if(S.charAt(i)==Alphabet.get(j)){
+                    char curr = S.charAt(i);
+                    res.add(i+"");
+                    Alphabet.remove(i);
+                    Alphabet.add(0,curr);
+                }
+            }
+        }
+        return res;
+    }
+
+    static ArrayList<Character>alphabet(){
+        ArrayList<Character>table = new ArrayList<>();
+        char cap='A';
+        for(int i = 0;i<26;i++){
+            table.add(cap++);
+        }
+        return table;
+    }
+
+    public static void print(){
+        for(int i = 0;i<26;i++){
+            System.out.println(Alphabet.get(i));
+        }
+    }
+}
